79900efc1d90d1dbcfd68faf9f72c42cfb10060d
TejaReddy-7 <152948008+TejaReddy-7@users.noreply.github.com>
2025-07-26 13:09:51 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import CandidateOnboarding from './components/CandidateOnboarding';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
+    <Dashboard />
     <CandidateOnboarding />
   );
 }
